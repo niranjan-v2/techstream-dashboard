cc54fc94e7526b7e711a14c534ecb47578ece1a6
Niranjan Vasudevan <niranjanv233@gmail.com>
2025-11-28 18:41:24 +0800

    Remove dummy notch

--- client/src/pages/VtsApp.jsx
+++ client/src/pages/VtsApp.jsx
@@ -6,9 +6,6 @@ export default function VtsApp() {
     <div className="min-h-screen bg-slate-950 flex items-center justify-center px-4 py-8">
       {/* Phone frame */}
       <div className="relative w-full max-w-sm aspect-[9/19] rounded-[2.5rem] bg-black shadow-2xl border border-slate-700 overflow-hidden">
-        {/* Notch */}
-        <div className="absolute top-3 left-1/2 -translate-x-1/2 w-24 h-5 rounded-full bg-slate-800 z-20" />
-
         {/* Embedded Expo web app */}
         <iframe
           title="Valluvan Tamil School App"
